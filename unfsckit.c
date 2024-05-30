5a13f38c60cf4ade76258e4613a35600be116852
Richard Campbell <rlcamp@users.noreply.github.com>
2024-05-30 14:08:07 -0700

    use sorear logf approx

--- unfsckit.c
+++ unfsckit.c
@@ -10,6 +10,13 @@ static float cmagsquaredf(const float complex x) {
     return crealf(x) * crealf(x) + cimagf(x) * cimagf(x);
 }
 
+static float logf_good_enough_approx(float x) {
+    /* thanks to stef o'rear for pointing out that ieee floats are already a first-order
+     approximation of logf when interpreted as an integer, you just have to scale and shift it */
+    const union { float f; uint32_t i; } y = { .f = x };
+    return y.i * (0.6931472f / 8388608.0f) - 127.0f * 0.6931472f;
+}
+
 static float complex renormalize(const float complex x) {
     /* assuming x is already near unity, renormalize to unity w/o div or sqrt */
     return x * (3.0f - cmagsquaredf(x)) * 0.5f;
@@ -50,8 +57,9 @@ static float argmax_of_fft_of_dechirped(float * power_max_p,
     const float prev = cmagsquaredf(fft_output[(iw_max + S - 1) % S]);
     const float next = cmagsquaredf(fft_output[(iw_max + 1) % S]);
 
-    /* these logarithms are very expensive */
-    const float alpha = logf(prev * one_over_this), gamma = logf(next * one_over_this);
+    /* actual logf is expensive, use a fast approx that is good enough for the use case */
+    const float alpha = logf_good_enough_approx(prev * one_over_this);
+    const float gamma = logf_good_enough_approx(next * one_over_this);
     const float p = 0.5f * (alpha - gamma) / (alpha + gamma);
 
     if (power_max_p) *power_max_p = max;
