7523609e6aef6767f3eb01175a51af940cae4789
Richard Campbell <rlcamp@users.noreply.github.com>
2025-06-25 13:14:02 -0700

    guard definition of struct iovec

--- fsckit.h
+++ fsckit.h
@@ -3,10 +3,13 @@
 #include <complex.h>
 
 /* standard posix definition of an iovec */
+#ifndef _STRUCT_IOVEC
+#define _STRUCT_IOVEC
 struct iovec {
     char * iov_base;
     size_t iov_len;
 };
+#endif
 
 float complex fsckit(void (* emit_sample_func)(void *, const int16_t), void * emit_sample_ctx,
                      const float amplitude, const float fs, const float fc, const float bw,
